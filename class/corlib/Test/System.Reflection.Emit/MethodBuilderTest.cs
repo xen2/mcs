xen2/mcs

commit 2f1e4f960b642739a3586993f65deb1910147f1f

Committing a patch from Kamil Skalski <nazgul@nemerle.org> containing some .NET 2.0 updates.
svn path=/trunk/mcs/; revision=45750

class/corlib/System.Reflection.Emit/FieldBuilder.cs
@@ -80,7 +80,7 @@ namespace System.Reflection.Emit {
 
 		public override RuntimeFieldHandle FieldHandle {
 			get {
-				throw CreateNotSupportedException ();
+				return handle;
 			}
 		}
 

class/corlib/System.Reflection.Emit/MethodBuilder.cs
@@ -126,7 +126,7 @@ namespace System.Reflection.Emit {
 
 		public override RuntimeMethodHandle MethodHandle {
 			get {
-				throw NotSupported ();
+				return mhandle;
 			}
 		}
 

class/corlib/System.Reflection.Emit/ConstructorBuilder.cs
@@ -120,7 +120,7 @@ namespace System.Reflection.Emit {
 
 		public override RuntimeMethodHandle MethodHandle { 
 			get {
-				throw not_supported ();
+				return mhandle;
 			}
 		}
 

class/corlib/System.Reflection.Emit/TypeBuilder.cs
@@ -1468,7 +1468,7 @@ namespace System.Reflection.Emit {
 
 		public override bool HasGenericArguments {
 			get {
-				throw new NotImplementedException ();
+				return generic_params != null;
 			}
 		}
 
@@ -1511,6 +1511,33 @@ namespace System.Reflection.Emit {
 		{
 			return DefineMethod (name, attributes, CallingConventions.Standard, null, null);
 		}
+
+                public static ConstructorInfo GetConstructor (Type instanciated, ConstructorInfo ctor)
+                {
+			ConstructorInfo res = instanciated.GetConstructor (ctor);
+			if (res == null)
+				throw new System.Exception ("constructor not found");
+			else
+				return res;
+                }
+
+                public static MethodInfo GetMethod (Type instanciated, MethodInfo meth)
+                {
+			MethodInfo res = instanciated.GetMethod (meth);
+			if (res == null)
+				throw new System.Exception ("method not found");
+			else
+				return res;
+                }
+
+                public static FieldInfo GetField (Type instanciated, FieldInfo fld)
+                {
+			FieldInfo res = instanciated.GetField (fld);
+			if (res == null)
+				throw new System.Exception ("field not found");
+			else
+				return res;
+                }
 #endif
 	}
 }

class/corlib/System.Reflection/MonoGenericClass.cs
@@ -57,6 +57,15 @@ namespace System.Reflection
 		protected extern void initialize (MethodInfo[] methods, ConstructorInfo[] ctors, FieldInfo[] fields, PropertyInfo[] properties, EventInfo[] events);
 
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
+		extern MethodInfo GetCorrespondingInflatedMethod (IntPtr generic);
+
+		[MethodImplAttribute(MethodImplOptions.InternalCall)]
+		extern ConstructorInfo GetCorrespondingInflatedConstructor (IntPtr generic);
+
+		[MethodImplAttribute(MethodImplOptions.InternalCall)]
+		extern FieldInfo GetCorrespondingInflatedField (IntPtr generic);
+
+		[MethodImplAttribute(MethodImplOptions.InternalCall)]
 		protected extern MethodInfo[] GetMethods_internal (Type reflected_type);
 
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
@@ -123,6 +132,27 @@ namespace System.Reflection
 			return generic_type.IsValueType;
 		}
 
+		internal override MethodInfo GetMethod (MethodInfo fromNoninstanciated)
+		{
+			initialize ();
+
+			return GetCorrespondingInflatedMethod (fromNoninstanciated.MethodHandle.Value);
+		}
+
+		internal override ConstructorInfo GetConstructor (ConstructorInfo fromNoninstanciated)
+		{
+			initialize ();
+
+			return GetCorrespondingInflatedConstructor (fromNoninstanciated.MethodHandle.Value);
+		}
+
+		internal override FieldInfo GetField (FieldInfo fromNoninstanciated)
+		{
+			initialize ();
+
+			return GetCorrespondingInflatedField (fromNoninstanciated.FieldHandle.Value);
+		}
+		
 		public override MethodInfo[] GetMethods (BindingFlags bf)
 		{
 			ArrayList l = new ArrayList ();

class/corlib/System/MonoType.cs
@@ -224,6 +224,31 @@ namespace System
 		}
 
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
+		extern MethodInfo GetCorrespondingInflatedMethod (IntPtr generic);
+
+		[MethodImplAttribute(MethodImplOptions.InternalCall)]
+		extern ConstructorInfo GetCorrespondingInflatedConstructor (IntPtr generic);
+
+		[MethodImplAttribute(MethodImplOptions.InternalCall)]
+		extern FieldInfo GetCorrespondingInflatedField (IntPtr generic);
+
+
+		internal override MethodInfo GetMethod (MethodInfo fromNoninstanciated)
+                {
+                        return GetCorrespondingInflatedMethod (fromNoninstanciated.MethodHandle.Value);
+                }
+
+		internal override ConstructorInfo GetConstructor (ConstructorInfo fromNoninstanciated)
+		{
+                        return GetCorrespondingInflatedConstructor (fromNoninstanciated.MethodHandle.Value);
+		}
+
+		internal override FieldInfo GetField (FieldInfo fromNoninstanciated)
+		{
+                        return GetCorrespondingInflatedField (fromNoninstanciated.FieldHandle.Value);
+		}
+
+		[MethodImplAttribute(MethodImplOptions.InternalCall)]
 		public extern override Type GetNestedType (string name, BindingFlags bindingAttr);
 
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]

class/corlib/System/Type.cs
@@ -760,6 +760,22 @@ namespace System {
 			return GetMethodImpl (name, bindingAttr, binder, callConvention, types, modifiers);
 		}
 
+		internal virtual MethodInfo GetMethod (MethodInfo fromNoninstanciated)
+                {
+			throw new System.InvalidOperationException ("can only be called in generic type");
+                }
+
+		internal virtual ConstructorInfo GetConstructor (ConstructorInfo fromNoninstanciated)
+                {
+			throw new System.InvalidOperationException ("can only be called in generic type");
+                }
+
+		internal virtual FieldInfo GetField (FieldInfo fromNoninstanciated)
+                {
+			throw new System.InvalidOperationException ("can only be called in generic type");
+                }
+
+		
 		public MethodInfo[] GetMethods ()
 		{
 			return GetMethods (DefaultBindingFlags);
@@ -1109,6 +1125,11 @@ namespace System {
 				throw new TypeLoadException ();
 			return res;
 		}
+
+                public Type MakeGenericType (Type[] types)
+                {
+			return BindGenericParameters (types);
+                }
 
 		public abstract bool IsGenericParameter {
 			get;

commit 257cb9047bc6b8595cd5cb4f2d65594edc3aeb22

* FieldBuilder.cs: FieldBuilder.FieldHandle should throw NotSupportException to match MS.NET (1.x and 2.x). Fixes regression introduce in r45750. * MethodBuilder.cs: MethodBuilder.MethodHandle should throw NotSupportedException to match MS.NET (1.x and 2.x). Fixes regression introduced in r45750. * MethodBuilderTest.cs: Enabled test for MethodHandle again after fixing regression, and added additional test. Added Test attributes to tests. * FieldBuilderTest.cs: Enabled tests for FieldHandle again after fixing regression. * ConstructorBuilderTest.cs: MethodHandle should not throw NotSupportedException.
svn path=/trunk/mcs/; revision=45816

class/corlib/System.Reflection.Emit/FieldBuilder.cs
@@ -80,7 +80,7 @@ namespace System.Reflection.Emit {
 
 		public override RuntimeFieldHandle FieldHandle {
 			get {
-				return handle;
+				throw CreateNotSupportedException ();
 			}
 		}
 

class/corlib/System.Reflection.Emit/MethodBuilder.cs
@@ -126,7 +126,7 @@ namespace System.Reflection.Emit {
 
 		public override RuntimeMethodHandle MethodHandle {
 			get {
-				return mhandle;
+				throw NotSupported ();
 			}
 		}
 

class/corlib/Test/System.Reflection.Emit/ConstructorBuilderTest.cs
@@ -94,8 +94,6 @@ public class ConstructorBuilderTest : Assertion
 	}
 	
 	[Test]
-	[Category ("NotWorking")]
-	[ExpectedException (typeof(NotSupportedException))]
 	public void TestMethodHandle () {
 		ConstructorBuilder cb = genClass.DefineConstructor (
 			 0, 0, new Type [0]);

class/corlib/Test/System.Reflection.Emit/FieldBuilderTest.cs
@@ -52,7 +52,6 @@ namespace MonoTests.System.Reflection.Emit
 		}
 
 		[Test]
-		[Category ("NotWorking")]
 		[ExpectedException (typeof(NotSupportedException))]
 		public void TestFieldHandleIncomplete ()
 		{
@@ -62,7 +61,6 @@ namespace MonoTests.System.Reflection.Emit
 		}
 
 		[Test]
-		[Category ("NotWorking")]
 		[ExpectedException (typeof(NotSupportedException))]
 		public void TestFieldHandleComplete ()
 		{

class/corlib/Test/System.Reflection.Emit/MethodBuilderTest.cs
@@ -59,6 +59,7 @@ public class MethodBuilderTest : Assertion
 		return "class" + (typeIndexer ++);
 	}
 
+	[Test]
 	public void TestAttributes () {
 		MethodBuilder mb = genClass.DefineMethod (
 			genMethodName (), MethodAttributes.Public, typeof (void), new Type [0]);
@@ -67,6 +68,7 @@ public class MethodBuilderTest : Assertion
 					  MethodAttributes.Public, mb.Attributes);
 	}
 
+	[Test]
 	public void TestCallingConvention () {
 		MethodBuilder mb = genClass.DefineMethod (
 			genMethodName (), 0, typeof (void), new Type[0]);
@@ -88,6 +90,7 @@ public class MethodBuilderTest : Assertion
 					  mb4.CallingConvention);
 	}
 
+	[Test]
 	public void TestDeclaringType () {
 		MethodBuilder mb = genClass.DefineMethod (
 			genMethodName (), 0, typeof (void), new Type[0]);
@@ -96,6 +99,7 @@ public class MethodBuilderTest : Assertion
 					  genClass, mb.DeclaringType);
 	}
 
+	[Test]
 	public void TestInitLocals () {
 		MethodBuilder mb = genClass.DefineMethod (
 			genMethodName (), 0, typeof (void), new Type[0]);
@@ -106,15 +110,26 @@ public class MethodBuilderTest : Assertion
 	}
 	
 	[Test]
-	[Category ("NotWorking")]
 	[ExpectedException (typeof(NotSupportedException))]
-	public void TestMethodHandle () {
+	public void TestMethodHandleIncomplete () {
 		MethodBuilder mb = genClass.DefineMethod (
 			genMethodName (), 0, typeof (void), new Type [0]);
 
 		RuntimeMethodHandle handle = mb.MethodHandle;
 	}
 
+	[Test]
+	[ExpectedException (typeof(NotSupportedException))]
+	public void TestMethodHandleComplete () {
+		MethodBuilder mb = genClass.DefineMethod (
+			genMethodName (), 0, typeof (void), new Type [0]);
+		mb.CreateMethodBody (new byte[2], 0);
+		genClass.CreateType ();
+
+		RuntimeMethodHandle handle = mb.MethodHandle;
+	}
+
+	[Test]
 	public void TestName () {
 		string name = genMethodName ();
 		MethodBuilder mb = genClass.DefineMethod (
@@ -123,6 +138,7 @@ public class MethodBuilderTest : Assertion
 		AssertEquals ("Name works", name, mb.Name);
 	}
 
+	[Test]
 	public void TestReflectedType () {
 		MethodBuilder mb = genClass.DefineMethod (
 			genMethodName (), 0, typeof (void), new Type [0]);
@@ -131,6 +147,7 @@ public class MethodBuilderTest : Assertion
 					  genClass, mb.ReflectedType);
 	}
 
+	[Test]
 	public void TestReturnType () {
 		MethodBuilder mb = genClass.DefineMethod (
 			genMethodName (), 0, typeof (Console), new Type [0]);
@@ -145,6 +162,7 @@ public class MethodBuilderTest : Assertion
 		Assert ("void ReturnType works", (mb2.ReturnType == null) || (mb2.ReturnType == typeof (void)));
 	}
 
+	[Test]
 	public void TestReturnTypeCustomAttributes () {
 		MethodBuilder mb = genClass.DefineMethod (
 			genMethodName (), 0, typeof (Console), new Type [0]);
@@ -162,6 +180,7 @@ public class MethodBuilderTest : Assertion
 	}
 	*/
 
+	[Test]
 	public void TestCreateMethodBody () {
 		MethodBuilder mb = genClass.DefineMethod (
 			genMethodName (), 0, typeof (void), new Type [0]);
@@ -217,6 +236,7 @@ public class MethodBuilderTest : Assertion
 			new Type[2] {
 			typeof(int), typeof(int)
 		});
+		mb.CreateMethodBody (new byte[2], 0);
 		tb.CreateType ();
 		mb.DefineParameter (-5, ParameterAttributes.None, "param1");
 	}
@@ -231,10 +251,12 @@ public class MethodBuilderTest : Assertion
 			new Type[2] {
 			typeof(int), typeof(int)
 		});
+		mb.CreateMethodBody (new byte[2], 0);
 		tb.CreateType ();
 		mb.DefineParameter (1, ParameterAttributes.None, "param1");
 	}
 
+	[Test]
 	public void TestDefineParameter () {
 		TypeBuilder tb = module.DefineType (genTypeName (), TypeAttributes.Public);
 		MethodBuilder mb = tb.DefineMethod (
@@ -288,6 +310,7 @@ public class MethodBuilderTest : Assertion
 			methodName.GetHashCode (), mb.GetHashCode ());
 	}
 
+	[Test]
 	public void TestGetBaseDefinition () {
 		MethodBuilder mb = genClass.DefineMethod (
 			genMethodName (), 0, typeof (void), new Type [0]);
@@ -296,6 +319,7 @@ public class MethodBuilderTest : Assertion
 					  mb.GetBaseDefinition (), mb);
 	}
 
+	[Test]
 	public void TestGetILGenerator () {
 		MethodBuilder mb = genClass.DefineMethod (
 			genMethodName (), 0, typeof (void), new Type [0]);
@@ -324,6 +348,7 @@ public class MethodBuilderTest : Assertion
 		}
 	}
 
+	[Test]
 	public void TestMethodImplementationFlags () {
 		TypeBuilder tb = module.DefineType (genTypeName (), TypeAttributes.Public);
 		MethodBuilder mb = tb.DefineMethod (
@@ -351,6 +376,7 @@ public class MethodBuilderTest : Assertion
 		}
 	}
 
+	[Test]
 	public void TestGetModule () {
 		MethodBuilder mb = genClass.DefineMethod (
 			genMethodName (), 0, typeof (void), new Type [0]);
@@ -359,6 +385,7 @@ public class MethodBuilderTest : Assertion
 					  mb.GetModule ());
 	}
 
+	[Test]
 	public void TestGetParameters () {
 		TypeBuilder tb = module.DefineType (genTypeName (), TypeAttributes.Public);
 		MethodBuilder mb = tb.DefineMethod (
@@ -380,6 +407,7 @@ public class MethodBuilderTest : Assertion
 		*/
 	}
 
+	[Test]
 	public void TestGetToken () {
 		TypeBuilder tb = module.DefineType (genTypeName (), TypeAttributes.Public);
 		MethodBuilder mb = tb.DefineMethod (
@@ -388,6 +416,7 @@ public class MethodBuilderTest : Assertion
 		mb.GetToken ();
 	}
 
+	[Test]
 	public void TestInvoke () {
 		MethodBuilder mb = genClass.DefineMethod (
 			genMethodName (), 0, typeof (void), 
@@ -406,23 +435,21 @@ public class MethodBuilderTest : Assertion
 		}
 	}
 
+	[Test]
+	[ExpectedException (typeof (NotSupportedException))]
 	public void TestIsDefined () {
 		MethodBuilder mb = genClass.DefineMethod (
 			genMethodName (), 0, typeof (void), 
 			new Type [1] {typeof(int)});
-
-		try {
-			mb.IsDefined (null, true);
-			Fail ();
-		} catch (NotSupportedException) {
-		}
+		mb.IsDefined (null, true);
 	}
 
+	[Test]
 	public void TestGetCustomAttributes () {
 		MethodBuilder mb = genClass.DefineMethod (
 			genMethodName (), 0, typeof (void), 
 			new Type [1] {typeof(int)});
-
+		
 		try {
 			mb.GetCustomAttributes (true);
 			Fail ();
@@ -436,6 +463,7 @@ public class MethodBuilderTest : Assertion
 		}
 	}
 
+	[Test]
 	public void TestSetCustomAttribute () {
 		TypeBuilder tb = module.DefineType (genTypeName (), TypeAttributes.Public);
 		string name = genMethodName ();