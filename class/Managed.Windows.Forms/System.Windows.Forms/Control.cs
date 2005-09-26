xen2/mcs

commit c90663c6d13a0aea92a850ee721fd0947976522f

2005-08-30  Peter Dennis Bartok  <pbartok@novell.com>
	* Form.cs: Set proper default for Opacity property
	* NotifyIcon.cs:
	  - ShowSystray(): Don't bother creating telling the OS
	    about the systray item if no icon is provided
	  - Now handles WM_NCPAINT message to deal with whole/client window
	    split
	  - Create window as visible to not get caught by Expose optimization
	* Hwnd.cs: Removed debug message
	* ComboBox.cs, ScrollBar.cs, ListBox.cs, TrackBar.cs, TabControl.cs,
	  StatusBar.cs, TreeView.cs, XplatUIOSX.cs, XplatUIWin32.cs: Switched 
	    PaintEventStart/End to use new client argument
	* TextBoxBase.cs:
	  - Commented out debug messages
	  - Switched PaintEventStart/End to use new client argument
	* XplatUI.cs: Added client window bool to PaintEventStart()/
	  PaintEventEnd() calls, to support drawing in non-client areas
	* XplatUIDriver.cs: 
	  - Added client window bool to PaintEventStart()/PaintEventEnd() 
	    calls, to support drawing in non-client areas
	  - Added conditional compile to allow using MWF BeginInvoke 
	    on MS runtime
	* XplatUIX11.cs:
	  - Added some conditional debug output
	  - Fixed SystrayAdd() method to support new (for SystrayAdd, anyway)
	    whole/client window split
	  - Implemented handling of client argument to PaintEventStart()/End()
	* Control.cs:
	  - Throw exception if BeginInvoke() is called and the window handle
	    or one of the window's parent handles is not created
	  - Added conditional compile to allow using MWF BeginInvoke on
	    MS runtime
	  - get_Parent(): Only sets parent if handle is created. This avoids
	    forcing window handle creation when parent is set.
	  - Now fires Layout and Parent changed events in proper order
	  - Switched to use Handle instead of window.Handle for Z-Order setting,
	    the get_Parent() patch above causes us to possibly get null for 'window'
	  - Implemented handling of client argument to PaintEventStart()/End()

class/Managed.Windows.Forms/System.Windows.Forms/Control.cs
@@ -631,9 +631,21 @@ namespace System.Windows.Forms
 		#endregion	// Internal Properties
 
 		#region Private & Internal Methods
-		internal static IAsyncResult BeginInvokeInternal (Delegate method, object [] args) {
-			AsyncMethodResult result = new AsyncMethodResult ();
-			AsyncMethodData data = new AsyncMethodData ();
+		internal IAsyncResult BeginInvokeInternal (Delegate method, object [] args) {
+			AsyncMethodResult	result;
+			AsyncMethodData		data;
+			Control			p;
+
+			p = this;
+			do {
+				if (!p.IsHandleCreated) {
+					throw new InvalidOperationException("Cannot call Invoke or InvokeAsync on a control until the window handle is created");
+				}
+				p = p.parent;
+			} while (p != null);
+
+			result = new AsyncMethodResult ();
+			data = new AsyncMethodData ();
 
 			data.Method = method;
 			data.Args = args;
@@ -644,9 +656,11 @@ namespace System.Windows.Forms
 				data.Context = ExecutionContext.Capture ();
 			}
 #else
+#if !MWF_ON_MSRUNTIME
 			if (SecurityManager.SecurityEnabled) {
 				data.Stack = CompressedStack.GetCompressedStack ();
 			}
+#endif
 #endif
 
 			XplatUI.SendAsyncMethod (data);
@@ -1686,11 +1700,12 @@ namespace System.Windows.Forms
 						parent.Controls.Add(this);
 					}
 
-					XplatUI.SetParent(Handle, value.Handle);
-
-					InitLayout();
+					if (IsHandleCreated) {
+						XplatUI.SetParent(Handle, value.Handle);
+					}
 
 					OnParentChanged(EventArgs.Empty);
+					InitLayout();
 				}
 			}
 		}
@@ -2530,8 +2545,16 @@ namespace System.Windows.Forms
 
 		public virtual void Refresh() {			
 			if (IsHandleCreated == true) {
+				int	end;
+
 				Invalidate();
 				XplatUI.UpdateWindow(window.Handle);
+
+				end = child_controls.Count;
+				for (int i=0; i < end; i++) {
+					child_controls[i].Refresh();
+				}
+				
 			}
 		}
 
@@ -3278,7 +3301,7 @@ namespace System.Windows.Forms
 #else
 			children = child_controls.Count;
 			for (int i = 1; i < children; i++ ) {
-				XplatUI.SetZOrder(child_controls[i].window.Handle, child_controls[i-1].window.Handle, false, false); 
+				XplatUI.SetZOrder(child_controls[i].Handle, child_controls[i-1].Handle, false, false); 
 			}
 #endif
 		}
@@ -3305,12 +3328,12 @@ namespace System.Windows.Forms
 				case Msg.WM_PAINT: {				
 					PaintEventArgs	paint_event;
 
-					paint_event = XplatUI.PaintEventStart(Handle);
+					paint_event = XplatUI.PaintEventStart(Handle, true);
 
 					if (!needs_redraw) {
 						// Just blit the previous image
 						paint_event.Graphics.DrawImage (ImageBuffer, paint_event.ClipRectangle, paint_event.ClipRectangle, GraphicsUnit.Pixel);
-						XplatUI.PaintEventEnd(Handle);
+						XplatUI.PaintEventEnd(Handle, true);
 						return;
 					}
 
@@ -3331,7 +3354,7 @@ namespace System.Windows.Forms
 						needs_redraw = false;
 					}
 
-					XplatUI.PaintEventEnd(Handle);
+					XplatUI.PaintEventEnd(Handle, true);
 
 					if (!GetStyle(ControlStyles.UserPaint)) {
 						DefWndProc(ref m);
@@ -3566,6 +3589,7 @@ namespace System.Windows.Forms
 
 					XplatUI.SetCursor(window.Handle, cursor.handle);
 					m.Result = (IntPtr)1;
+
 					return;
 				}
 

class/Managed.Windows.Forms/System.Windows.Forms/ComboBox.cs
@@ -910,9 +910,9 @@ namespace System.Windows.Forms
 
 			case Msg.WM_PAINT: {
 				PaintEventArgs	paint_event;
-				paint_event = XplatUI.PaintEventStart (Handle);
+				paint_event = XplatUI.PaintEventStart (Handle, true);
 				OnPaintCB (paint_event);
-				XplatUI.PaintEventEnd (Handle);
+				XplatUI.PaintEventEnd (Handle, true);
 				return;
 			}
 

class/Managed.Windows.Forms/System.Windows.Forms/Form.cs
@@ -72,7 +72,7 @@ namespace System.Windows.Forms {
 		private SizeGripStyle		size_grip_style;
 		private Rectangle		maximized_bounds;
 		private Rectangle		default_maximized_bounds;
-		private double			opacity = 1.0;
+		private double			opacity;
 		Color				transparency_key;
 
 		#endregion	// Local Variables
@@ -115,6 +115,7 @@ namespace System.Windows.Forms {
 			start_position = FormStartPosition.WindowsDefaultLocation;
 			formBorderStyle = FormBorderStyle.Sizable;
 			key_preview = false;
+			opacity = 1D;
 			menu = null;
 			icon = null;
 			minimum_size = new Size(0, 0);

class/Managed.Windows.Forms/System.Windows.Forms/Hwnd.cs
@@ -96,7 +96,6 @@ namespace System.Windows.Forms {
 				Marshal.FreeHGlobal((IntPtr)marshal_free_list[i]);
 			}
 			marshal_free_list.Clear();
-Console.WriteLine("Disposing window {0:X} (whole: {1:X})", client_window.ToInt32(), whole_window.ToInt32());
 		}
 		#endregion
 

class/Managed.Windows.Forms/System.Windows.Forms/ListBox.cs
@@ -973,9 +973,9 @@ namespace System.Windows.Forms
 
 			case Msg.WM_PAINT: {
 				PaintEventArgs	paint_event;
-				paint_event = XplatUI.PaintEventStart (Handle);
+				paint_event = XplatUI.PaintEventStart (Handle, true);
 				OnPaintLB (paint_event);
-				XplatUI.PaintEventEnd (Handle);
+				XplatUI.PaintEventEnd (Handle, true);
 				return;
 			}
 

class/Managed.Windows.Forms/System.Windows.Forms/NotifyIcon.cs
@@ -81,6 +81,7 @@ namespace System.Windows.Forms {
 					cp.Parent = IntPtr.Zero;
 					cp.Style = (int)WindowStyles.WS_POPUP;
 					cp.Style |= (int)WindowStyles.WS_CLIPSIBLINGS;
+					cp.Style |= (int)WindowStyles.WS_VISIBLE;
 
 					cp.ExStyle = (int)(WindowStyles.WS_EX_TOOLWINDOW);
 
@@ -90,6 +91,16 @@ namespace System.Windows.Forms {
 
 			protected override void WndProc(ref Message m) {
 				switch((Msg)m.Msg) {
+					case Msg.WM_NCPAINT: {
+						PaintEventArgs	paint_event;
+
+						
+						paint_event = XplatUI.PaintEventStart(Handle, false);
+						OnPaint(paint_event);
+						XplatUI.PaintEventEnd(Handle, false);
+						break;
+					}
+
 					case Msg.WM_USER: {
 						switch ((Msg)m.LParam.ToInt32()) {
 							case Msg.WM_LBUTTONDOWN: {
@@ -232,9 +243,11 @@ namespace System.Windows.Forms {
 				return;
 			}
 
-			if (icon != null) {
-				icon_bitmap = icon.ToBitmap();
+			if (icon == null) {
+				return;
 			}
+
+			icon_bitmap = icon.ToBitmap();
 
 			systray_active = true;
 			XplatUI.SystrayAdd(window.Handle, text, icon, out tooltip);

class/Managed.Windows.Forms/System.Windows.Forms/ScrollBar.cs
@@ -487,9 +487,9 @@ namespace System.Windows.Forms
 				{
 					PaintEventArgs	paint_event;
 
-					paint_event = XplatUI.PaintEventStart (Handle);
+					paint_event = XplatUI.PaintEventStart (Handle, true);
 					OnPaintSB (paint_event);
-					XplatUI.PaintEventEnd (Handle);
+					XplatUI.PaintEventEnd (Handle, true);
 					return;
 				}
 

class/Managed.Windows.Forms/System.Windows.Forms/StatusBar.cs
@@ -277,9 +277,9 @@ namespace System.Windows.Forms {
 				case Msg.WM_PAINT: {				
 					PaintEventArgs	paint_event;
 
-					paint_event = XplatUI.PaintEventStart (Handle);
+					paint_event = XplatUI.PaintEventStart (Handle, true);
 					DoPaint (paint_event);
-					XplatUI.PaintEventEnd (Handle);
+					XplatUI.PaintEventEnd (Handle, true);
 					return;
 				}
 			}

commit 3ad614077aaedb63bef1190c675c94c0509ce9d6

2005-09-26  Peter Dennis Bartok  <pbartok@novell.com>
	* Control.cs: Fixed SetChildIndex; it no longer causes a child to
	  loose it's parent. (Fixed bug introduced in r49103 when we added
	  setting the child parent to null on Remove)


svn path=/trunk/mcs/; revision=50806

class/Managed.Windows.Forms/System.Windows.Forms/Control.cs
@@ -427,6 +427,7 @@ namespace System.Windows.Forms
 				} else {
 					list.Insert(newIndex, child);
 				}
+				child.parent = owner;
 				owner.UpdateZOrder();
 			}
 			#endregion // ControlCollection Private Instance Methods