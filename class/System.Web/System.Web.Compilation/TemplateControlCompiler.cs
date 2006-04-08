xen2/mcs

commit 29b1c8a07ddeed6916c27a6e07a848c75e913284

2006-03-24 Gonzalo Paniagua Javier <gonzalo@ximian.com>
	* System.Web.UI/ControlBuilder.cs: when creating a default property
	builder, handle strings specially.

	* System.Web.UI/StringPropertyBuilder.cs: new builder used in
	TextBox 2.0.
	* System.Web.dll.sources: add StringPropertyBuilder.
	* System.Web.Compilation/TemplateControlCompiler.cs: handle the new
	StringPropertyBuilder.

	Fixes bug #77762.


svn path=/trunk/mcs/; revision=58505

class/System.Web/System.Web.Compilation/TemplateControlCompiler.cs
@@ -981,6 +981,19 @@ namespace System.Web.Compilation
 						continue;
 					}
 
+					if (b is StringPropertyBuilder) {
+						StringPropertyBuilder pb = (StringPropertyBuilder) b;
+						string str = (pb.Children.Count > 0) ? (string) pb.Children [0] : null;
+						if (str != null) {
+							CodeMemberMethod method = builder.method;
+							CodeAssignStatement assign = new CodeAssignStatement ();
+							assign.Left = new CodePropertyReferenceExpression (ctrlVar, pb.PropertyName);
+							assign.Right = new CodePrimitiveExpression (str);
+							method.Statements.Add (assign);
+						}
+						continue;
+					}
+
 #if NET_2_0
 					if (b is ContentBuilderInternal) {
 						ContentBuilderInternal cb = (ContentBuilderInternal) b;

class/System.Web/System.Web.UI/ControlBuilder.cs
@@ -351,6 +351,8 @@ namespace System.Web.UI {
 				builder = new CollectionBuilder ();
 			} else if (typeof (ITemplate).IsAssignableFrom (propType)) {
 				builder = new TemplateBuilder (prop);
+			} else if (typeof (string) == propType) {
+				builder = new StringPropertyBuilder (prop.Name);
 			} else {
 				builder = CreateBuilderFromType (parser, parentBuilder, propType, prop.Name,
 								 null, atts, line, fileName);

class/System.Web/System.Web.UI/StringPropertyBuilder.cs
@@ -1,0 +1,53 @@
+//
+// System.Web.UI.StringPropertyBuilder.cs
+//
+// Authors:
+// 	Gonzalo Paniagua Javier (gonzalo@ximian.com)
+//
+// Copyright (c) 2006 Novell, Inc (http://www.novell.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+using System.Web;
+namespace System.Web.UI {
+	class StringPropertyBuilder : ControlBuilder {
+		string prop_name;
+
+		public StringPropertyBuilder (string prop_name)
+		{
+			this.prop_name = prop_name;
+		}
+
+		public string PropertyName {
+			get { return prop_name; }
+		}
+
+		public override bool AllowWhitespaceLiterals ()
+		{
+			return false;
+		}
+
+		public override void AppendSubBuilder (ControlBuilder subBuilder)
+		{
+			throw new HttpException ("StringPropertyBuilder should never be called");
+		}
+	}
+}
+

class/System.Web/System.Web.dll.sources
@@ -605,6 +605,7 @@ System.Web.UI/StateBag.cs
 System.Web.UI/StateItem.cs
 System.Web.UI/StateManagedCollection.cs
 System.Web.UI/StaticPartialCachingControl.cs
+System.Web.UI/StringPropertyBuilder.cs
 System.Web.UI/SupportsEventValidationAttribute.cs
 System.Web.UI/TagPrefixAttribute.cs
 System.Web.UI/TemplateBuilder.cs

commit 16465ee8d459ea4a9fc22b57900abb9865be754e

2006-04-08  Miguel de Icaza  <miguel@novell.com>
	* TemplateControlCompiler.cs: An attempt to fix the regression
	introduced in r58505 (a bug fix for 77762).   This was reported in
	the mailing list with a batch of new 2.0 failures.

	We really need a test suite in NUnit to check on ASP.NET aspx

svn path=/trunk/mcs/; revision=59249

class/System.Web/Makefile
@@ -72,7 +72,7 @@ endif
 
 LIB_MCS_FLAGS = \
 	-unsafe \
-	-nowarn:612,649	\
+	-nowarn:612,618,649	\
 	-r:$(corlib)	 		\
 	-r:System.dll 			\
 	-r:System.Drawing.dll 		\

class/System.Web/System.Web.Compilation/TemplateControlCompiler.cs
@@ -981,9 +981,9 @@ namespace System.Web.Compilation
 						continue;
 					}
 
-					if (b is StringPropertyBuilder) {
-						StringPropertyBuilder pb = (StringPropertyBuilder) b;
-						string str = (pb.Children.Count > 0) ? (string) pb.Children [0] : null;
+					StringPropertyBuilder pb = b as StringPropertyBuilder;
+					if (pb != null){
+						string str = (pb.Children != null && pb.Children.Count > 0) ? (string) pb.Children [0] : null;
 						if (str != null) {
 							CodeMemberMethod method = builder.method;
 							CodeAssignStatement assign = new CodeAssignStatement ();