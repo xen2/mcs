xen2/mcs

commit b166b8932768130501c6b689abaf09422289d3a6

2006-05-23  Chris Toshok  <toshok@ximian.com>
	* ThemeWin32Classic.cs (DataGridPaintColumsHdrs): intersect the
	header rectangle with the clip rectangle so we don't redraw the
	entire header for just a small area.  Gets rid of the last flicker
	when horizontally scrolling.
	(DataGridPaintRow): same.


svn path=/trunk/mcs/; revision=61019

class/Managed.Windows.Forms/System.Windows.Forms/ThemeWin32Classic.cs
@@ -903,8 +903,6 @@ namespace System.Windows.Forms
 			rect_columnhdr.Y = columns_area.Y;
 			rect_columnhdr.Height = columns_area.Height;
 			
-			current_clip = new Region (columns_area);
-			g.Clip = current_clip;
 			int column_cnt = grid.first_visiblecolumn + grid.visiblecolumn_count;
 			for (int column = grid.first_visiblecolumn; column < column_cnt; column++) {
 				
@@ -915,12 +913,15 @@ namespace System.Windows.Forms
 				if (clip.IntersectsWith (rect_columnhdr) == false)
 					continue;
 
+				current_clip = new Region (rect_columnhdr);
+				current_clip.Intersect (clip);
+				g.Clip = current_clip;
+
 				grid.CurrentTableStyle.GridColumnStyles[column].PaintHeader (g, rect_columnhdr, column);
 
-				
+				current_clip.Dispose ();
 			}
 
-			current_clip.Dispose ();
 			g.ResetClip ();
 				
 			Rectangle not_usedarea = columnshdrs_area_complete;				
@@ -1114,8 +1115,8 @@ namespace System.Windows.Forms
 				rect_cell.Width = grid.CurrentTableStyle.GridColumnStyles[column].Width;
 
 				if (clip.IntersectsWith (rect_cell)) {
-					current_clip = new Region (row_rect);
-					current_clip.Intersect (rect_cell);
+					current_clip = new Region (rect_cell);
+					current_clip.Intersect (clip);
 					g.Clip = current_clip;
 
 					if (is_newrow) {

commit 45e691dab607c00ce93d8684081b043b4b1700a4

2006-05-25  Chris Toshok  <toshok@ximian.com>
	* ThemeWin32Classic.cs: fix datagrid clipping problems caused by
	r61019.


svn path=/trunk/mcs/; revision=61137

class/Managed.Windows.Forms/System.Windows.Forms/ThemeWin32Classic.cs
@@ -914,6 +914,7 @@ namespace System.Windows.Forms
 					continue;
 
 				current_clip = new Region (rect_columnhdr);
+				current_clip.Intersect (columns_area);
 				current_clip.Intersect (clip);
 				g.Clip = current_clip;
 
@@ -1116,6 +1117,7 @@ namespace System.Windows.Forms
 
 				if (clip.IntersectsWith (rect_cell)) {
 					current_clip = new Region (rect_cell);
+					current_clip.Intersect (row_rect);
 					current_clip.Intersect (clip);
 					g.Clip = current_clip;
 