xen2/mcs

commit 8da57e8578f2918b6dea3b40b27ac54338082de2

2007-03-27  Everaldo Canuto  <everaldo@simios.org>
	* XplatUIWin32.cs:
	- NIF_STATE and NIF_INFO added to NotifyIconFlags.
	- NOTIFYICONDATA properties sizes fixed, szTip is 128, not 64.
	- SystrayBalloon method implemented.
	[Add support for notifyicon balloon on win32, #79149]


svn path=/trunk/mcs/; revision=75048

class/Managed.Windows.Forms/System.Windows.Forms/XplatUIWin32.cs
@@ -586,6 +586,8 @@ namespace System.Windows.Forms {
 			NIF_MESSAGE			= 0x00000001,
 			NIF_ICON			= 0x00000002,
 			NIF_TIP				= 0x00000004,
+			NIF_STATE			= 0x00000008,
+			NIF_INFO			= 0x00000010			
 		}
 
 		[StructLayout(LayoutKind.Sequential, CharSet=CharSet.Unicode)]
@@ -596,7 +598,7 @@ namespace System.Windows.Forms {
 			internal NotifyIconFlags	uFlags;
 			internal uint				uCallbackMessage;
 			internal IntPtr				hIcon;
-			[MarshalAs(UnmanagedType.ByValTStr, SizeConst=64)]
+			[MarshalAs(UnmanagedType.ByValTStr, SizeConst=128)]
 			internal string				szTip;
 			internal int				dwState;
 			internal int				dwStateMask;
@@ -605,10 +607,7 @@ namespace System.Windows.Forms {
 			internal int				uTimeoutOrVersion;
 			[MarshalAs(UnmanagedType.ByValTStr, SizeConst=64)]
 			internal string				szInfoTitle;
-			internal int				dwInfoFlags;
-			// Win Vista only
-			//internal IntPtr guidItem;      
-			//internal IntPtr hBalloonIcon;
+			internal ToolTipIcon		dwInfoFlags;
 		}
 
 		[Flags]
@@ -2291,7 +2290,20 @@ namespace System.Windows.Forms {
 #if NET_2_0
 		internal override void SystrayBalloon(IntPtr hwnd, int timeout, string title, string text, ToolTipIcon icon)
 		{
-			// TODO:
+			NOTIFYICONDATA	nid;
+
+			nid = new NOTIFYICONDATA();
+
+			nid.cbSize = (uint)Marshal.SizeOf(nid);
+			nid.hWnd = hwnd;
+			nid.uID = 1;
+			nid.uFlags = NotifyIconFlags.NIF_INFO;
+			nid.uTimeoutOrVersion = timeout;
+			nid.szInfoTitle = title;
+			nid.szInfo = text;
+			nid.dwInfoFlags = icon;
+			
+			Win32Shell_NotifyIcon(NotifyIconMessage.NIM_MODIFY, ref nid);
 		}
 #endif
 

commit 2e54ae16f3ec9abf199dd62b97811aa1f3bbc187

Fix build breakage in r75048
svn path=/trunk/mcs/; revision=75052

class/Managed.Windows.Forms/System.Windows.Forms/XplatUIWin32.cs
@@ -607,7 +607,11 @@ namespace System.Windows.Forms {
 			internal int				uTimeoutOrVersion;
 			[MarshalAs(UnmanagedType.ByValTStr, SizeConst=64)]
 			internal string				szInfoTitle;
+#if NET_2_0
 			internal ToolTipIcon		dwInfoFlags;
+#else
+			internal int				dwInfoFlags;
+#endif
 		}
 
 		[Flags]