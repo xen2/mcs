xen2/mcs

commit 2313e41542ee3728ee3cd3fd3a331c42b51be356

2009-07-22  Atsushi Enomoto  <atsushi@ximian.com>
	* SerializationMap.cs : for empty element, do not try to consume
	  children. Fixed bug #524086.
	  For enum, it is always error when the enum is not flags, and
	  is always default value for flags.

	* XmlObjectSerializerTest.cs :
	  Add test for bug #524086 by Rolf Bjarne Kvinge.
	  Add enum flags test, which should not raise an error.


svn path=/trunk/mcs/; revision=138386

class/System.Runtime.Serialization/Test/System.Runtime.Serialization/XmlObjectSerializerTest.cs
@@ -839,6 +839,13 @@ namespace MonoTests.System.Runtime.Serialization
 		}
 
 		[Test]
+		public void DeserializeEnumFlags ()
+		{
+			Deserialize<Colors2> (
+				@"<Colors2 xmlns=""http://schemas.datacontract.org/2004/07/MonoTests.System.Runtime.Serialization""/>");
+		}
+
+		[Test]
 		public void DeserializeEnumWithDC ()
 		{
 			ColorsWithDC cdc = Deserialize<ColorsWithDC> (
@@ -1206,6 +1213,28 @@ namespace MonoTests.System.Runtime.Serialization
 		}
 
 		[Test]
+		public void EmptyChildren ()
+		{
+                string xml = @"
+<DummyPlaylist xmlns='http://example.com/schemas/asx'>
+        <Entries>
+                <DummyEntry>
+                        <EntryInfo xmlns:i='http://www.w3.org/2001/XMLSchema-ins
+tance' i:type='PartDummyEntryInfo'/>
+                        <Href>http://vmsservices.example.com:8080/VideoService.s
+vc?crid=45541/part=1/guid=ae968b5d-e4a5-41fe-9b23-ed631b27cd21/</Href>
+                </DummyEntry>
+        </Entries>
+</DummyPlaylist>
+";
+			var reader = XmlReader.Create (new StringReader (xml));
+			DummyPlaylist playlist = (DummyPlaylist) new DataContractSerializer (typeof (DummyPlaylist)).ReadObject (reader);
+			Assert.AreEqual (1, playlist.entries.Count, "#1");
+			Assert.IsTrue (playlist.entries [0] is DummyEntry, "#2");
+			Assert.IsNotNull (playlist.entries [0].Href, "#3");
+		}
+
+		[Test]
 		public void BaseKnownTypeAttributes ()
 		{
 			// bug #524088
@@ -1252,6 +1281,11 @@ namespace MonoTests.System.Runtime.Serialization
  	}
  
 	public enum Colors {
+		Red, Green, Blue
+	}
+
+	[Flags]
+	public enum Colors2 {
 		Red, Green, Blue
 	}
 
@@ -1530,16 +1564,18 @@ public class MyDictionary<K,V> : Dictionary<K,V>
 {
 }
 
-// bug #524088
+// bug #524086
 [DataContract(Namespace="http://example.com/schemas/asx")]
 public class DummyEntry
 {
     [DataMember]
     public DummyEntryInfo EntryInfo { get; set; }
+    [DataMember]
+    public string Href { get; set; }
 }
 
-[DataContract(Namespace="http://example.com/schemas/asx")]
-[KnownType(typeof(PartDummyEntryInfo))]
+[DataContract(Namespace="http://example.com/schemas/asx"),
+KnownType(typeof(PartDummyEntryInfo))]
 public abstract class DummyEntryInfo
 {
 }
@@ -1547,8 +1583,10 @@ public abstract class DummyEntryInfo
 [DataContract(Namespace="http://example.com/schemas/asx")]
 public class DummyPlaylist
 {
+    public IList<DummyEntry> entries = new List<DummyEntry> ();
+
     [DataMember]
-    public IList<DummyEntry> Entries { get; set; }
+    public IList<DummyEntry> Entries { get { return entries; } set {entries = value;} }
 }
 
 [DataContract(Namespace="http://example.com/schemas/asx")]
@@ -1556,6 +1594,8 @@ public class PartDummyEntryInfo : DummyEntryInfo
 {
     public PartDummyEntryInfo() {}
 }
+
+// bug #524088
 
 [DataContract(Namespace="http://example.com/schemas/asx")]
 public class AsxEntryInfo

class/System.Runtime.Serialization/System.Runtime.Serialization/SerializationMap.cs
@@ -363,17 +363,23 @@ namespace System.Runtime.Serialization
 		public virtual object DeserializeEmptyContent (XmlReader reader,
 			XmlFormatterDeserializer deserializer)
 		{
-			return DeserializeContent (reader, deserializer);
+			return DeserializeContent (reader, deserializer, true);
 		}
 
 		public virtual object DeserializeContent (XmlReader reader,
 			XmlFormatterDeserializer deserializer)
 		{
+			return DeserializeContent (reader, deserializer, false);
+		}
+
+		object DeserializeContent (XmlReader reader,
+			XmlFormatterDeserializer deserializer, bool empty)
+		{
 			object instance = FormatterServices.GetUninitializedObject (RuntimeType);
 			int depth = reader.NodeType == XmlNodeType.None ? reader.Depth : reader.Depth - 1;
 			bool [] filled = new bool [Members.Count];
 			int memberInd = -1;
-			while (reader.NodeType == XmlNodeType.Element && reader.Depth > depth) {
+			while (!empty && reader.NodeType == XmlNodeType.Element && reader.Depth > depth) {
 				DataMemberInfo dmi = null;
 				int i = 0;
 				for (; i < Members.Count; i++) { // unordered
@@ -961,6 +967,7 @@ namespace System.Runtime.Serialization
 	internal class EnumMap : SerializationMap
 	{
 		List<EnumMemberInfo> enum_members;
+		bool flag_attr;
 
 		public EnumMap (
 			Type type, QName qname, KnownTypeCollection knownTypes)
@@ -971,6 +978,7 @@ namespace System.Runtime.Serialization
 				typeof (DataContractAttribute), false);
 			if (atts.Length != 0)
 				has_dc = true;
+			flag_attr = type.GetCustomAttributes (typeof (FlagsAttribute), false).Length > 0;
 
 			enum_members = new List<EnumMemberInfo> ();
 			BindingFlags flags = BindingFlags.Public | BindingFlags.DeclaredOnly | BindingFlags.Static;
@@ -1047,6 +1055,14 @@ namespace System.Runtime.Serialization
 				"Enum value '{0}' is invalid for type '{1}' and cannot be serialized.", graph, RuntimeType));
 		}
 
+		public override object DeserializeEmptyContent (XmlReader reader,
+			XmlFormatterDeserializer deserializer)
+		{
+			if (!flag_attr)
+				throw new SerializationException (String.Format ("Enum value '' is invalid for type '{0}' and cannot be deserialized.", RuntimeType));
+			return Enum.ToObject (RuntimeType, 0);
+		}
+
 		public override object DeserializeContent (XmlReader reader,
 			XmlFormatterDeserializer deserializer)
 		{
@@ -1058,8 +1074,9 @@ namespace System.Runtime.Serialization
 						return emi.Value;
 			}
 
-			throw new SerializationException (String.Format (
-				"Enum value '{0}' is invalid for type '{1}' and cannot be deserialized.", value, RuntimeType));
+			if (!flag_attr)
+				throw new SerializationException (String.Format ("Enum value '{0}' is invalid for type '{1}' and cannot be deserialized.", value, RuntimeType));
+			return Enum.ToObject (RuntimeType, 0);
 		}
 	}
 

commit 03a974d77844f095a857ea66579dd733f89dc53b

2009-07-23  Sebastien Pouliot  <sebastien@ximian.com>
	* XmlObjectSerializerTest.cs: Add test cases with a generic
	dictionary (empty, broken with r138386, and with one element) 
	that both works on MS.NET and for which Moonlight depends on.


svn path=/trunk/mcs/; revision=138531

class/System.Runtime.Serialization/Test/System.Runtime.Serialization/XmlObjectSerializerTest.cs
@@ -1278,6 +1278,37 @@ vc?crid=45541/part=1/guid=ae968b5d-e4a5-41fe-9b23-ed631b27cd21/</Href>
 			Assert.AreEqual (runtimeType, o.GetType (), "#DS0");
 			return (T)o;
 		}
+
+		public Dictionary<string, object> GenericDictionary (Dictionary<string, object> settings)
+		{
+			using (MemoryStream ms = new MemoryStream ()) {
+				DataContractSerializer save = new DataContractSerializer (settings.GetType ());
+				save.WriteObject (ms, settings);
+
+				ms.Position = 0;
+
+				DataContractSerializer load = new DataContractSerializer (typeof (Dictionary<string, object>));
+				return (Dictionary<string, object>) load.ReadObject (ms);
+			}
+		}
+
+		[Test]
+		public void GenericDictionaryEmpty ()
+		{
+			Dictionary<string, object> in_settings = new Dictionary<string, object> ();
+			Dictionary<string, object> out_settings = GenericDictionary (in_settings);
+			out_settings.Clear ();
+		}
+
+		[Test]
+		public void GenericDictionaryOneElement ()
+		{
+			Dictionary<string, object> in_settings = new Dictionary<string, object> ();
+			in_settings.Add ("one", "ONE");
+			Dictionary<string, object> out_settings = GenericDictionary (in_settings);
+			Assert.AreEqual ("ONE", out_settings ["one"], "out");
+			out_settings.Clear ();
+		}
  	}
  
 	public enum Colors {