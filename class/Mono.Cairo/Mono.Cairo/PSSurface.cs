mono/mono

Mono.Cairo: Fix names of native functions used in PSSurface
It seems cairo_ps_surface_begin_* functions never existed, they were
always named cairo_ps_surface_dsc_begin_*.

mcs/class/Mono.Cairo/Mono.Cairo/PSSurface.cs
@@ -46,12 +46,12 @@ namespace Cairo {
 
 		public void BeginPageSetup ()
 		{
-			NativeMethods.cairo_ps_surface_begin_page_setup (surface);
+			NativeMethods.cairo_ps_surface_dsc_begin_page_setup (surface);
 		}
 
 		public void BeginSetup ()
 		{
-			NativeMethods.cairo_ps_surface_begin_setup (surface);
+			NativeMethods.cairo_ps_surface_dsc_begin_setup (surface);
 		}
 
 		public void DscComment (string comment)

xen2/mcs

Conflicts:
	mcs/class/Mono.Cairo/Mono.Cairo/PSSurface.cs

class/Mono.Cairo/Mono.Cairo/PSSurface.cs
@@ -43,12 +43,12 @@ namespace Cairo {
 
 		public void BeginPageSetup ()
 		{
-			NativeMethods.cairo_ps_surface_begin_page_setup (Handle);
+			NativeMethods.cairo_ps_surface_dsc_begin_page_setup (Handle);
 		}
 
 		public void BeginSetup ()
 		{
-			NativeMethods.cairo_ps_surface_begin_setup (Handle);
+			NativeMethods.cairo_ps_surface_dsc_begin_setup (Handle);
 		}
 
 		public void DscComment (string comment)